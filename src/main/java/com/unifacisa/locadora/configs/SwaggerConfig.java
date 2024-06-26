b60969d2c9efd70dd7ea21c26a6834816435fdfe
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2024-06-26 18:58:29 -0300

    melhoria na documentação swagger

---
+++ src/main/java/com/unifacisa/locadora/configs/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.unifacisa.locadora.configs;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+git
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Locadora API")
+                        .description("API para gerenciamento de filmes na locadora")
+                        .version("0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentação da API da Locadora"));
+    }
+
+
+}
