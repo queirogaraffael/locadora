7c174f0b8af392f6c3fabd1575f40492aa814a9d
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2024-09-14 16:58:46 -0300

    teste de carga/performance com k6

--- load_test.js
+++ load_test.js
@@ -4,31 +4,35 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '30s', target: 5000 },
-        { duration: '1m', target: 5000 },
-        { duration: '30s', target: 9000 },
-        { duration: '2m', target: 9000 },
-        { duration: '30s', target: 0 },
+        { duration: '2m', target: 5000 }, 
+        { duration: '5m', target: 5000 }, 
+        { duration: '3m', target: 9000 }, 
+        { duration: '3m', target: 9000 }, 
+        { duration: '1m', target: 0 },
     ],
+    
 };
 
 
 export default function () {
 
+    let id = Math.floor(Math.random() * 5) + 1;
 
     // Pega todos os dados básicos de um filme
-    let res = http.get('http://localhost:8080/filmes/1');
+    let res = http.get(`http://localhost:8080/filmes/${id}`);
 
     check(res, {
         'status was 200': (r) => r.status === 200,
+        'response time is < 500ms': (r) => r.timings.duration < 500,
     });
 
 
     // Pega todas as categorias de um filme
-    res = http.get('http://localhost:8080/filmes/categorias/1');
+    res = http.get(`http://localhost:8080/filmes/categorias/${id}`);
 
     check(res, {
         'status was 200': (r) => r.status === 200,
+        'response time is < 500ms': (r) => r.timings.duration < 500,
     });
 
 
